c53240badd492a189b546ffedddd3221aac21a26
erickgfs <erickgfs@gmail.com>
2024-10-20 03:52:08 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
+
+import { GlobalStyle } from './styles/globals';
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
+
+export default App;
